254d7d0fd32eb843ffd6f0eea4bf1ce383908261
Alexsik76 <alex@jurist.vn.ua>
2020-10-05 17:12:52 +0300

    set color under 15

--- flask_app/app/flask_module/main.py
+++ flask_app/app/flask_module/main.py
@@ -4,8 +4,13 @@ from flask_app.app.flask_module.race_table import get_html_report
 app = Flask(__name__)
 
 
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
 @app.route('/report')
-def index():
+def report():
     column_names = {'Position': 'pos',
                     'Abbreviation': 'abr',
                     'Name': 'name',
@@ -14,7 +19,7 @@ def index():
                     'Finish time': 'finish',
                     'Race time': 'race_time'}
     racers = get_html_report()
-    return render_template('race_table.html', records=racers, colnames=column_names)
+    return render_template('report.html', records=racers, colnames=column_names)
 
 
 if __name__ == '__main__':
