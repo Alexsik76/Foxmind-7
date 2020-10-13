564007a1d50e12d69a7fbe9111a9b58858c1d72d
Alexsik76 <alex@jurist.vn.ua>
2020-10-13 10:38:00 +0300

    work with race_table.py

--- app/race_table.py
+++ app/race_table.py
@@ -32,11 +32,19 @@ def parsing_line(line: tuple) -> list:
     :rtype: list[str, str, str, datetime, datetime, datetime]
     """
     f_time = '%Y-%m-%d_%H:%M:%S.%f'
+    t_spec = "milliseconds"
+    pos = None
     abr, name, team = line[0].split('_')
     start = datetime.strptime(line[1][3:], f_time)
     finish = datetime.strptime(line[2][3:], f_time)
     race_time = datetime.min + abs(finish - start)
-    return [abr, name, team, start, finish, race_time]
+    return [pos,
+            abr,
+            name,
+            team,
+            start.time().isoformat(t_spec),
+            finish.time().isoformat(t_spec),
+            race_time.time().isoformat(t_spec)]
 
 
 def get_report() -> list:
@@ -52,29 +60,10 @@ def get_report() -> list:
                         read_file(files['start']),
                         read_file(files['finish']))
     racers = sorted([parsing_line(line) for line in source_racers],
-                    key=lambda lst: lst[5])
+                    key=lambda lst: lst[6])
     for number, item in enumerate(racers, start=1):
-        item.append(number)
-    return racers
-
-
-def get_racer_html(racer: list) -> namedtuple:
-    """Converts data about each racer into a convenient text format.
-
-    :param racer: A list of the racers data.
-    :type racer: list
-    :return: A named tuple of the the racers data.
-    :rtype: namedtuple.
-    """
-    t_spec = "milliseconds"
-    driver = Racer(str(racer[6]),
-                   racer[0],
-                   racer[1],
-                   racer[2],
-                   str(racer[3].time().isoformat(t_spec)),
-                   str(racer[4].time().isoformat(t_spec)),
-                   str(racer[5].time().isoformat(t_spec)))
-    return driver
+        item[0] = number
+    return [Racer(*racer) for racer in racers]
 
 
 def get_html_report(sort: str = 'ASC') -> list:
@@ -86,10 +75,7 @@ def get_html_report(sort: str = 'ASC') -> list:
     :return: A list of strings of the the racers data.
     :rtype: list.
     """
-    racers = get_report()
-    racers_html = []
-    for racer in racers:
-        racers_html.append(get_racer_html(racer))
+    racers_html = get_report()
     if sort.lower() == 'desc':
         racers_html.reverse()
     return racers_html
