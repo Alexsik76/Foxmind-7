49bb91477247201fa8f121b58cacb7570c30f9b5
Alexsik76 <alex@jurist.vn.ua>
2020-11-01 23:36:46 +0200

    finished tests

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -9,3 +9,23 @@ def test_config():
 def test_hello(client):
     response = client.get('/hello')
     assert response.data == b'Hello, World!'
+
+
+def test_report(client):
+    response = client.get('/report/')
+    assert b'Daniel Ricciardo' in response.data
+
+
+def test_report_drivers(client):
+    response = client.get('/report/drivers/')
+    assert b'Daniel Ricciardo' in response.data
+
+
+def test_error(client):
+    response = client.get('/report/drivers/?driver_id=AAA')
+    assert b'Driver not found' in response.data
+
+
+def test_get_driver(client):
+    response = client.get('/report/drivers/?driver_id=SVF')
+    assert b'Sebastian Vettel' in response.data
